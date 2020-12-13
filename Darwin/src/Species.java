743bd06d6cf5ddaa5bc888e44f31755d0a4218f4
Z. Gamal <63029433+zgamal@users.noreply.github.com>
2020-12-13 06:03:40 +0200

    Darwin preliminary classes (not final)

---
+++ Darwin/src/Species.java
@@ -0,0 +1,148 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+/**
+ * The individual creatures in the world are all representatives of some
+ * species class and share certain common characteristics, such as the species
+ * name and the program they execute. Rather than copy this information into
+ * each creature, this data is recorded once as part of the description for
+ * a species and then each creature can simply include the appropriate species
+ * pointer as part of its internal data structure.
+ * <p>
+ * 
+ * To encapsulate all of the operations operating on a species within this
+ * abstraction, this provides a constructor that will read a file containing
+ * the name of the creature and its program, as described in the earlier part
+ * of this assignment. To make the folder structure more manageable, the
+ * species files for each creature are stored in a subfolder named Creatures.
+ * Thus, creating the Species for the file Hop.txt will causes the program to
+ * read in "Creatures/Hop.txt".
+ * 
+ * <p>
+ * 
+ * Note: The instruction addresses start at one, not zero.
+ */
+
+public class Species {
+	
+	ArrayList<Instruction> program = new ArrayList<Instruction>();
+	ArrayList<String> sprogram = new ArrayList<String>();
+	String name;
+	String color;
+	
+	/**
+	 * Create a species for the given file. 
+	 * @param fileName the name of the file containing the data for the species
+	 * @pre fileName exists in the Creature subdirectory.
+	 */
+	public Species(String fileName) {
+		try {
+		      File f = new File(fileName);
+		      Scanner s = new Scanner(f);
+		      int index = 0;
+		      while (s.hasNextLine()) {
+		    	sprogram.add(s.nextLine());
+		        if (index == 0) {
+		        	String l = s.nextLine();
+		        	name = l;
+		        	index++;
+		        } else if (index == 1) {
+		        	String l = s.nextLine();
+		        	color = l;
+		        	index++;
+		        } else {
+		        	String l = s.nextLine();
+		        	if (l.contains("hop")) {
+		        		Instruction instruction = new Instruction(Instruction.HOP, 0);
+		        		program.add(instruction);
+		        	} else if (l.contains("left")) {
+		        		Instruction instruction = new Instruction(Instruction.LEFT, 0);
+		        		program.add(instruction);
+		        	} else if (l.contains("right")) {
+		        		Instruction instruction = new Instruction(Instruction.RIGHT, 0);
+		        		program.add(instruction);
+		        	} else if (l.contains("infect")) {
+		        		Instruction instruction = new Instruction(Instruction.INFECT, 0);
+		        		program.add(instruction);
+		        	} else if (l.contains("ifempty")) {
+		        		String[] line = l.split(" ");
+		        		int address = (int) Integer.parseInt(line[1]);
+		        		Instruction instruction = new Instruction(Instruction.IFEMPTY, address);
+		        		program.add(instruction);
+		        	} else if (l.contains("ifwall")) {
+		        		String[] line = l.split(" ");
+		        		int address = (int) Integer.parseInt(line[1]);
+		        		Instruction instruction = new Instruction(Instruction.IFWALL, address);
+		        		program.add(instruction);
+		        	} else if (l.contains("ifsame")) {
+		        		String[] line = l.split(" ");
+		        		int address = (int) Integer.parseInt(line[1]);
+		        		Instruction instruction = new Instruction(Instruction.IFSAME, address);
+		        		program.add(instruction);
+		        	} else if (l.contains("ifenemy")) {
+		        		String[] line = l.split(" ");
+		        		int address = (int) Integer.parseInt(line[1]);
+		        		Instruction instruction = new Instruction(Instruction.IFENEMY, address);
+		        		program.add(instruction);
+		        	} else if (l.contains("ifrandom")) {
+		        		String[] line = l.split(" ");
+		        		int address = (int) Integer.parseInt(line[1]);
+		        		Instruction instruction = new Instruction(Instruction.IFRANDOM, address);
+		        		program.add(instruction);
+		        	} else if (l.contains("go")) {
+		        		String[] line = l.split(" ");
+		        		int address = (int) Integer.parseInt(line[1]);
+		        		Instruction instruction = new Instruction(Instruction.GO, address);
+		        		program.add(instruction);
+		        	} 
+		        }
+		        
+		      }
+		      s.close();
+		    } catch (FileNotFoundException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+	}
+	
+	/**
+	 * Return the name of the species.
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Return the color of the species.
+	 */
+	public String getColor() {
+		return color;
+	}
+
+	/**
+	 * Return the number of instructions in the program.
+	 */
+	public int programSize() {
+		return program.size();
+	}
+
+	/**
+	 * Return an instruction from the program. 
+	 * @pre 1 <= i <= programSize().
+	 * @post returns instruction i of the program.
+	 */
+	public Instruction programStep(int i) {
+		return program.get(i);
+	}
+
+	/**
+	 * Return a String representation of the program.
+	 */
+	public String programToString() {
+		return sprogram.toString();
+	}
+
+}
