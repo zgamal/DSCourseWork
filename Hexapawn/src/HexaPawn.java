68f1e9e355b84c4453abb5394456a764d9c6e4c2
Z. Gamal <63029433+zgamal@users.noreply.github.com>
2021-04-20 20:48:24 -0400

    Final Hexapawn with TQs

--- Hexapawn/src/HexaPawn.java
+++ Hexapawn/src/HexaPawn.java
@@ -1,3 +1,27 @@
+/* THOUGHT QUESTIONS 
+ * 
+ * 1. For a 3x4 board, there are 6003 positions, and for a 3x5 board, there are 215150 positions. Those numbers can be determined using a static int to
+ *    be incremented when a new GameTree (and board) is constructed.
+ * 
+ * 2. Based on testing, I think that H.E.R will ultimately win more frequently. This could  suggest that H.I.M kept trimming its trees as expected but 
+ *	  was unable to catch up with H.E.R, which, by moving second, has a greater chance of learning the perfect way to win. By moving second, H.E.R is 
+ *    able to force itself on H.I.M and dictate the flow of the game early on, where the board state becomes completely dependent on its movements. 
+ *    Thus, H.E.R can rapidly figure out the best and quickest path down the tree that would guarantee a win. As shown by the tests, after a few rounds H.E.R 
+ *    learns the best paths for each H.I.M move and begins to dominate over it. However, for large boards, H.I.M’s chances improve. In the 3x4 board, 
+ *    H.I.M won about 800/1000, but, on the 4x3 board, the relationship swapped again and HER won about 900/1000. When increasing the dimensions to 3x5
+ *    and 5x3, the same relationship is seen. This could be due to the fact that the larger the board dimensions, the longer it takes to find the 
+ *    optimum strategy.  
+ * 
+ * 3. The most efficient way to implement this would be that when the board is symmetric (3x3 board where left and right columns are identical), store
+ *    only one set of the children’s moves and add a boolean instance in the GameTree class called mirror that would keep track if a certain board is a
+ *    mirror of another. If it is, then using another method, that GameTree node would reference the mirrored one, such that when it calls any
+ *    function, the code for the mirror can be reused on the current reflection of the node. And so, we only need to have on GameTree object that would
+ *    be a reference to the other one when mirror is true. Finally, we would need a method to print the mirrored version when mirror is true by
+ *    iterating through and swapping the positions as necessary to their mirrored correct one.
+ * 
+ */
+
+
 import structure5.*;
 import java.util.Scanner;
 
