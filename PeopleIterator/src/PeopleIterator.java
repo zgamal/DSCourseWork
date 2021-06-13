3f3a44d7ade52df7a14ebc2a6243314521d1cbf7
Z. Gamal <63029433+zgamal@users.noreply.github.com>
2021-06-13 17:36:38 -0400

    Add files via upload

---
+++ PeopleIterator/src/PeopleIterator.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class PeopleIterator<E> implements Iterator<E> {
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		PeopleIterator<E> pi = new PeopleIterator<E>();
+		
+	}
+
+}
